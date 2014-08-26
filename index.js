3ff6684243535ccff119fc992314b7d154355c2a
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-08-25 22:49:10 -0700

    Reimplement sampleCode using SKRJ basis from pomagma

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     compiler: require('./lib/compiler'),
+    grammar: require('./lib/grammar'),
     pretty: require('./lib/pretty'),
     tree: require('./lib/tree'),
     cursor: require('./lib/cursor'),
