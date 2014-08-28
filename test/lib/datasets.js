21215b983225f9821feed7a0afb95fcf56905899
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-08-28 00:54:51 -0700

    Add terms and nodes test datasets

--- test/lib/datasets.js
+++ test/lib/datasets.js
@@ -2,20 +2,20 @@
 
 var stats = require('../../lib/stats');
 var grammar = require('../../lib/grammar');
-//var _ = require('underscore');
-//var compiler = require('../../lib/compiler');
-//var tree = require('../../lib/tree');
+var _ = require('underscore');
+var compiler = require('../../lib/compiler');
+var tree = require('../../lib/tree');
 
 var SAMPLE_COUNT = 1000;
 
 var codes = stats.sampleUnique(grammar.sampleCode, SAMPLE_COUNT);
-//var terms = _.map(codes, compiler.load);
+var terms = _.map(codes, compiler.load);
 //var lines = _.map(codes, compiler.loadLine);
-//var nodes = _.map(terms, tree.load);
+var nodes = _.map(terms, tree.load);
 
 module.exports = {
     codes: codes,
-//    terms: terms,
-//    lines: lines,
-//    nodes: nodes,
+    terms: terms,
+    //lines: lines,
+    nodes: nodes,
 };
